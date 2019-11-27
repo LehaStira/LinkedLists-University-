e8bfaeb9d6d82127d350ea6b67ea34fcea05f1a0
Alexey <58273234+LehaStira@users.noreply.github.com>
2019-11-27 23:51:26 +0300

    Add files via upload

---
+++ LinkedList/Node.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAlgorithmsApp
+{
+    public class Node<T>
+    {
+       /* private T data;
+        private Node<T> next;
+
+       public Node(T data, Node<T> next=null)
+        {
+           this.data = data;
+           this.next = next;
+        }
+
+        /// <summary>
+        /// ссылка на следующий элемент
+        /// </summary>
+        public Node<T> Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+        /// <summary>
+        /// данные узла
+        /// </summary>
+        public T Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }*/
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SimpleAlgorithmsApp
+{
+    class Program
+    {
+        static string Action(string s)
+        {
+            char begin = s[s.Length - 4];
+            Console.WriteLine(begin);
+            s = s.Remove(s.IndexOf(begin), 3);
+            return s;
+        }
+        static bool CheckForLength(string s)
+        {
+            if (s.Length <= 4)
+            {
+                return true;
+            }
+            else
+            {
+               
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string s = "";
+            List<String> slova = new List<string>();
+            List<String> changeSl = new List<string>(); // новый, измененный список
+            using (StreamReader sr = new StreamReader("1.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    s = sr.ReadLine();
+                    string[] words = s.Split(' ', '.');
+                    foreach (string word in words)                // чтение из файла
+                    {
+                        // Console.WriteLine(word);
+                        slova.Add(word);
+                    }
+                }
+                sr.Close();
+            }
+
+            Console.WriteLine("Прочитали из файла:");
+
+
+            foreach (var item in slova)
+            {
+
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+            // выводим элементы
+            foreach (var item in slova)
+            {
+                if (CheckForLength(item) == true)         //////////////////////////////здесь применять функцию    
+                {
+                    slova.Remove(item);  // удаляем элемент
+                }
+            }
+            
+            Console.WriteLine("After deleting:");
+            Console.WriteLine();
+            foreach (var item in changeSl)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in slova)
+            {
+               changeSl.Add(Action(item));
+            }
+            Console.WriteLine("After changes:");
+            Console.WriteLine();
+            foreach (var item in changeSl)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
